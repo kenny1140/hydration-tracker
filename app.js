b03c70efcde993d03c4949199c87dab84188171f
Kenny <kennynoverian@gmail.com>
2026-08-25 01:04:43 +0200

    feat: complete water intake calculation and dynamic UI updates

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ let currentInTake = 0;
 
 const display = document.getElementById("progress-display");
 const btnContainer = document.getElementById("button-container");
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add-btn")) {
+    const amount = Number(e.target.dataset.amount);
+    currentInTake += amount;
+    display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
+    console.log(amount);
+  }
+});
