6fb6eb637d24594399a42db72d8e4001935a8ba2
Kenny <kennynoverian@gmail.com>
2026-08-27 01:39:07 +0200

    feat: add reset intake button and subtle styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let currentInTake = 0;
 
 const display = document.getElementById("progress-display");
 const btnContainer = document.getElementById("button-container");
+const resetBtn = document.getElementById("reset-btn");
 
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("add-btn")) {
@@ -11,4 +12,9 @@ btnContainer.addEventListener("click", (e) => {
     display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
     console.log(amount);
   }
+});
+
+resetBtn.addEventListener("click", () => {
+  currentInTake = 0;
+  display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
 });
