e7de33194cf89244fa1bd66cb606bbd006fd55e0
Kenny <kennynoverian@gmail.com>
2026-08-28 20:14:59 +0200

    refractor: clean up event listener and centralize UI updates.

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const dailyGoal = 2000;
-let currentInTake = 0;
+let currentInTake = JSON.parse(localStorage.getItem("waterIntake")) || 0;
 
 const display = document.getElementById("progress-display");
+const progressBar = document.getElementById("progress-bar");
 const btnContainer = document.getElementById("button-container");
 const resetBtn = document.getElementById("reset-btn");
+
+// Function to update both screen text, progress bar width, & Local Storage
+function updateUI() {
+  display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
+
+  // Calculate percentage
+  const percentage = Math.min((currentInTake / dailyGoal) * 100, 100);
+  progressBar.style.width = `${percentage}%`;
+
+  // Save current intake to Local Storage as JSOn
+  localStorage.setItem("waterIntake", JSON.stringify(currentInTake));
+}
+
+updateUI(); // Initial UI update on page load
 
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("add-btn")) {
     const amount = Number(e.target.dataset.amount);
     currentInTake += amount;
-    display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
     console.log(amount);
+    updateUI();
   }
 });
 
 resetBtn.addEventListener("click", () => {
   currentInTake = 0;
-  display.textContent = `${currentInTake} mL / ${dailyGoal} mL`;
+  updateUI();
 });
